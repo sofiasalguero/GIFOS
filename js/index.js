f3f7cf070e5fa801e5d0eb4c76eaa12c61488887
Sofia Salguero <sofiasalguero_09@hotmail.com>
2020-09-16 22:40:51 -0300

    Botones Imagenes

--- js/index.js
+++ js/index.js
@@ -2,9 +2,12 @@ let darkMode= false;
 const btnSwitch = document.getElementById('switch');
 const bodyClassList = document.body.classList;
 const navbarClassList = document.getElementById('navbar').classList;
+const appendButtons = document.getElementById('append-buttons').cloneNode(true);
+document.getElementById('append-buttons').remove();
 
 const newGiphyItem = (gif) => {
     const li = document.createElement('li');
+    li.classList.add('botones');
     const img = gif.images.original;
     const isFavorite = existsGif(gif.id);
     if (isFavorite) {
@@ -14,11 +17,13 @@ const newGiphyItem = (gif) => {
         li.addEventListener('click', () => addFavorito(gif.id));
     }
     const imgNode = document.createElement("img");
+    imgNode.classList.add('img-guia');
     imgNode.setAttribute('src', img.url);
     imgNode.setAttribute('width', img.width);
     imgNode.setAttribute('height', img.height);
     imgNode.setAttribute('alt', gif.title);
     li.appendChild(imgNode);
+    li.appendChild(appendButtons.cloneNode(true));
     return li;
 };
 
